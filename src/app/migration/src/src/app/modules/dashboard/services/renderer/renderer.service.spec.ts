5180420e6c09a61167ae64337d94af1e42f1ee1a
Nilesh More <nilesh_m@tekditechnologies.com>
2018-02-21 10:57:16 +0530

    Issue #SB-1485 fix: documentation

--- src/app/migration/src/src/app/modules/dashboard/services/renderer/renderer.service.spec.ts
+++ src/app/migration/src/src/app/modules/dashboard/services/renderer/renderer.service.spec.ts
@@ -1,16 +1,16 @@
-// import { LineChartService } from './graphs/line-chart.service';
-// import { TestBed, inject } from '@angular/core/testing';
+import { LineChartService } from './graphs/line-chart.service';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { RendererService } from './renderer.service';
+import { RendererService } from './renderer.service';
 
-// describe('RendererService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [RendererService, LineChartService]
-//     });
-//   });
+describe('RendererService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RendererService, LineChartService]
+    });
+  });
 
-//   it('should be created', inject([RendererService], (service: RendererService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([RendererService], (service: RendererService) => {
+    expect(service).toBeTruthy();
+  }));
+});
